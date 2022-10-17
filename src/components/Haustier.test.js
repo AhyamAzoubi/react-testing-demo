94b97de7a2bcc7e4758bbe106583b5b30347ea48
Tun Huang <huang.tun.anton@gmail.com>
2022-10-17 13:57:12 +0200

    Erstelle zweite Thumbnail-Regel

--- src/components/Haustier.test.js
+++ src/components/Haustier.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/render-result-naming-convention */
 import Haustier from "./Haustier.js";
 import { render } from "@testing-library/react";
 
@@ -6,4 +8,11 @@ it("displays a default thumbnail", async () => {
 
   const petThumbnail = await haustier.findByTestId("thumbnail");
   expect(petThumbnail.src).toContain("DCI");
+});
+
+it("displays a non-default thumbnail", async () => {
+  const haustier = render(<Haustier images={["1.jpg", "2.png", "3.webm"]}/>);
+
+  const petThumbnail = await haustier.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("1.jpg");
 });

003218ad3670ab4f366d7e0b18fb0735fe99fc2f
Tun Huang <huang.tun.anton@gmail.com>
2022-10-17 14:17:14 +0200

    Repariere Haustiertest mit StaticRouter

--- src/components/Haustier.test.js
+++ src/components/Haustier.test.js
@@ -2,16 +2,17 @@
 /* eslint-disable testing-library/render-result-naming-convention */
 import Haustier from "./Haustier.js";
 import { render } from "@testing-library/react";
+import { StaticRouter } from 'react-router-dom/server';
 
 it("displays a default thumbnail", async () => {
-  const haustier = render(<Haustier />);
+  const haustier = render(<StaticRouter><Haustier /></StaticRouter>);
 
   const petThumbnail = await haustier.findByTestId("thumbnail");
   expect(petThumbnail.src).toContain("DCI");
 });
 
 it("displays a non-default thumbnail", async () => {
-  const haustier = render(<Haustier images={["1.jpg", "2.png", "3.webm"]}/>);
+  const haustier = render(<StaticRouter><Haustier images={["1.jpg", "2.png", "3.webm"]}/></StaticRouter>);
 
   const petThumbnail = await haustier.findByTestId("thumbnail");
   expect(petThumbnail.src).toContain("1.jpg");

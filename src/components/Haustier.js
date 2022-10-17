84f2d1a3085ab928caa27f441180de6fe80c8d13
Tun Huang <huang.tun.anton@gmail.com>
2022-10-17 13:49:37 +0200

    Erstelle Test für Standard-Thumbnail

--- src/components/Haustier.js
+++ src/components/Haustier.js
@@ -5,13 +5,13 @@ import './Haustier.css';
 const Haustier = (props) => {
   let hero = "https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/612/original/DCI__logo_wordmark_blue.png";
 
-  if (props.images.length > 0) {
+  if (Array.isArray(props.images) && props.images.length > 0) {
     hero = props.images[0];
   }
 
   return (
     <div className="haustier-container">
-      <img src={hero} alt={props.name} width="300" />
+      <img src={hero} alt={props.name} width="300" data-testid="thumbnail" />
       <div>
         <h1>{props.name}</h1>
         <h2>{props.animal}</h2>
--- src/components/Haustier.test.js
+++ src/components/Haustier.test.js
@@ -1,6 +1,9 @@
 import Haustier from "./Haustier.js";
 import { render } from "@testing-library/react";
 
-// it("displays a default thumbnail", async () => {
+it("displays a default thumbnail", async () => {
+  const haustier = render(<Haustier />);
 
-// });
+  const petThumbnail = await haustier.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("DCI");
+});
